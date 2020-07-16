a59a61e4db5faf6a9742fb2a6724f87ae0567846
瓦姆乌·湫曗 <autumnsilverleaves@gmail.com>
2020-07-16 14:59:32 +0800

    use vite to dev runtime-dom

---
+++ packages/runtime-dom/src/index.ts
@@ -0,0 +1,5 @@
+import { welcome } from "./welcome";
+import { greeting } from "@vue-lite/runtime-core";
+
+console.log(greeting);
+console.log(welcome);
---
+++ packages/runtime-dom/src/welcome.ts
@@ -0,0 +1,1 @@
+export const welcome = "greeting: runtime-dom!";
