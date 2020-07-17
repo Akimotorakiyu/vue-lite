f4a21632c7970bedba1a36201412ca18e321a79b
瓦姆乌·湫曗 <autumnsilverleaves@gmail.com>
2020-07-18 00:46:31 +0800

    chore: add test for ProxyMap

--- packages/reactivity/__test__/map.test.ts
+++ packages/reactivity/__test__/map.test.ts
@@ -1,16 +1,16 @@
 import { reactive, effect, computed } from "../src/index";
 
-describe("reactive proxy array", () => {
+describe("reactive proxy Map", () => {
   test("reactive", () => {
-    const values = reactive(new Set<number>());
+    const values = reactive(new Map<number, number>());
 
-    values.add(0);
+    values.set(0, 0);
 
     expect(Array.from(values.values())[0]).toBe(0);
     expect(Array.from(values.keys())[0]).toBe(0);
     expect(Array.from(values.entries())[0][1]).toBe(0);
 
-    values.add(1);
+    values.set(1, 1);
 
     expect(Array.from(values.values())[1]).toBe(1);
     expect(Array.from(values.keys())[1]).toBe(1);
@@ -18,10 +18,11 @@ describe("reactive proxy array", () => {
   });
 
   test("reactive-effect", () => {
-    const values = reactive(new Set<number>());
+    const values = reactive(new Map<number, number>());
 
-    values.add(0);
-    values.add(1);
+    values.set(0, 0);
+
+    values.set(1, 1);
 
     let size = 0;
 
@@ -29,16 +30,17 @@ describe("reactive proxy array", () => {
       size = values.size;
     });
 
-    values.add(1);
+    values.set(1, 1);
 
     expect(size).toBe(values.size);
   });
 
   test("reactive-effect", () => {
-    const values = reactive(new Set<number>());
+    const values = reactive(new Map<number, number>());
 
-    values.add(0);
-    values.add(1);
+    values.set(0, 0);
+
+    values.set(1, 1);
     values.delete(0);
 
     let size = 0;
@@ -47,17 +49,18 @@ describe("reactive proxy array", () => {
       size = values.size;
     });
 
-    values.add(1);
+    values.set(1, 1);
 
     expect(size).toBe(values.size);
     expect(Array.from(values.values())[0]).toBe(1);
   });
 
   test("reactive-effect", () => {
-    const values = reactive(new Set<number>());
+    const values = reactive(new Map<number, number>());
 
-    values.add(0);
-    values.add(1);
+    values.set(0, 0);
+
+    values.set(1, 1);
 
     let size = 0;
 
@@ -65,26 +68,26 @@ describe("reactive proxy array", () => {
       size = values.size;
     });
 
-    values.add(2);
+    values.set(2, 2);
 
     values.clear();
     expect(size).toBe(values.size);
   });
 
   test("reactive-effect", () => {
-    const values = reactive(new Set<number>());
+    const values = reactive(new Map<number, number>());
 
-    values.add(0);
-    values.add(1);
+    values.set(0, 0);
+
+    values.set(1, 1);
 
     let has = false;
 
     effect(() => {
-      console.log("atttttt", values.has(2));
       has = values.has(2);
     });
 
-    values.add(2);
+    values.set(2, 2);
     expect(has).toBe(true);
 
     values.clear();
