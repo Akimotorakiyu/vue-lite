0ab2a7b293d3615aba3b54e382154732101e0752
瓦姆乌·湫曗 <autumnsilverleaves@gmail.com>
2020-07-18 00:36:24 +0800

    chore: add test for ProxySet

--- packages/reactivity/__test__/set.test.ts
+++ packages/reactivity/__test__/set.test.ts
@@ -7,25 +7,89 @@ describe("reactive proxy array", () => {
     values.add(0);
 
     expect(Array.from(values.values())[0]).toBe(0);
+    expect(Array.from(values.keys())[0]).toBe(0);
+    expect(Array.from(values.entries())[0][1]).toBe(0);
 
     values.add(1);
 
     expect(Array.from(values.values())[1]).toBe(1);
+    expect(Array.from(values.keys())[1]).toBe(1);
+    expect(Array.from(values.entries())[1][1]).toBe(1);
   });
 
-  //   test("reactive-effect", () => {
-  //     const values = reactive([1, 2, 3, 4]);
+  test("reactive-effect", () => {
+    const values = reactive(new Set<number>());
 
-  //     let value;
+    values.add(0);
+    values.add(1);
 
-  //     effect(() => {
-  //       value = values[1];
-  //     });
+    let size = 0;
 
-  //     values[1] = 0;
+    effect(() => {
+      size = values.size;
+    });
 
-  //     expect(value).toBe(0);
-  //   });
+    values.add(1);
+
+    expect(size).toBe(values.size);
+  });
+
+  test("reactive-effect", () => {
+    const values = reactive(new Set<number>());
+
+    values.add(0);
+    values.add(1);
+    values.delete(0);
+
+    let size = 0;
+
+    effect(() => {
+      size = values.size;
+    });
+
+    values.add(1);
+
+    expect(size).toBe(values.size);
+    expect(Array.from(values.values())[0]).toBe(1);
+  });
+
+  test("reactive-effect", () => {
+    const values = reactive(new Set<number>());
+
+    values.add(0);
+    values.add(1);
+
+    let size = 0;
+
+    effect(() => {
+      size = values.size;
+    });
+
+    values.add(2);
+
+    values.clear();
+    expect(size).toBe(values.size);
+  });
+
+  test("reactive-effect", () => {
+    const values = reactive(new Set<number>());
+
+    values.add(0);
+    values.add(1);
+
+    let has = false;
+
+    effect(() => {
+      console.log("atttttt", values.has(2));
+      has = values.has(2);
+    });
+
+    values.add(2);
+    expect(has).toBe(true);
+
+    values.clear();
+    expect(has).toBe(false);
+  });
 });
 
 export {};
