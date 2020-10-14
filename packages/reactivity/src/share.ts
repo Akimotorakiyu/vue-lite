b3ef378054894ad3970377960502c903fc25049d
瓦姆乌·湫曗 <autumnsilverleaves@gmail.com>
2020-10-14 10:16:56 +0800

    chore: use in instead ?:

--- packages/reactivity/src/share.ts
+++ packages/reactivity/src/share.ts
@@ -26,13 +26,13 @@ export function isArrayIndex(key: ProxyHandlerKey): key is ArrayIndex {
 export function isReactiveObject<T extends object>(
   target: T | ReactiveObject<T>
 ): target is ReactiveObject<T> {
-  return target[reactiveObjectKey] ? true : false;
+  return reactiveObjectKey in target;
 }
 
 export function isReactiveEffect<T, A extends []>(
   value: ReactiveEffect<T, A> | ((...args: A) => T)
 ): value is ReactiveEffect<T, A> {
-  return (value as ReactiveEffect<T, A>)?._isEffect ? true : false;
+  return "_isEffect" in value;
 }
 
 // ReactiveObject到原始值
