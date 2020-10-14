a7a43c7bdcf4346266f65db3ae8702e19ced3af5
瓦姆乌·湫曗 <autumnsilverleaves@gmail.com>
2020-10-14 11:38:18 +0800

    up docs

---
+++ packages/runtime-dom-lit-html/vite.config.ts
@@ -0,0 +1,12 @@
+import type { UserConfig } from "vite";
+// import i18n from "@intlify/vite-plugin-vue-i18n";
+
+const config: UserConfig = {
+  vueCustomBlockTransforms: {
+    // i18n,
+  },
+  outDir: "../../docs/",
+  base: "./vue-lite",
+};
+
+export default config;
